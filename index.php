e2a1dc7ab63c93a932af742c76f3e641ecfb5802
aaliyu <ayindealiyu1@gmail.com>
2022-10-13 21:45:39 +0200

    Update

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-use Aayinde\JsonOffice\Reader\JsonStringReader;
-
-require_once 'vender/autoload.php';
-
-$json_tester = new JsonStringReader();
-var_dump($json_tester->process());
-var_dump($json_tester->error);
